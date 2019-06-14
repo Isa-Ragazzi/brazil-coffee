3b7fbc07bade1f6292a3bd652b4111704acb2dc6
Fizzah Shaikh <shaikh.f@husky.neu.edu>
2019-06-13 22:24:29 -0700

    added in my attempet at munging

---
+++ coffee/munge/attempt2.R
@@ -0,0 +1,20 @@
+library(rgdal)
+library(raster)
+library(sp)
+
+#r = raster("tmin01")
+r <- getData("worldclim", var="bio", res=10)
+
+r <- r[[c(1,12)]]
+
+names(r) <- c("Temp","Precip","")
+
+points <- spsample(as(r@extent, 'SpatialPolygons'), n=100, type="random")
+
+values <- extract(r, points)
+
+df <- cbind.data.frame(coordinates(points), values)
+
+summary (df)
+
+plot(df)
