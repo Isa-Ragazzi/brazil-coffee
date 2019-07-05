41e0a5732026d57d3e05d72f424f53d854857f19
Fizzah Shaikh <shaikh.f@husky.neu.edu>
2019-07-05 10:22:30 -0700

    table of avgs from clim 5

---
+++ coffee/munge/table_avgs_1.R
@@ -0,0 +1,16 @@
+library(tmap)
+library(sf)
+library(raster)
+library(dplyr)
+library(spData)
+library(spDataLarge)
+library(brazilmaps)
+## import climate scenarios
+HG_model <- getData('CMIP5', var='bio', res=10, rcp=85, model='CN', year=50)
+BC_model <- getData('CMIP5', var='bio', res=10, rcp=85, model='BC', year=50)
+
+##map brazil
+bra <- get_brmap(geo ="Region",
+          geo.filter = NULL,
+          class = "sf")
+plot_brmap(bra)
